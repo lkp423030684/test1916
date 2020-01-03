ec6beda608ebe422833641ddcfc769daa91222ee
zs <zs@bjpowernode.com>
2020-01-03 21:36:29 +0800

    添加了bran22

--- src/main/java/com/bjpowernode/domain/Person.java
+++ src/main/java/com/bjpowernode/domain/Person.java
@@ -4,8 +4,8 @@ package com.bjpowernode.domain;
  * 梁坤鹏
  * 2020/1/3
  */
-@SuppressWarnings("MagicConstant")
 public class Person {
     private String id;
     private String naem;
+    private String bran22;
 }
